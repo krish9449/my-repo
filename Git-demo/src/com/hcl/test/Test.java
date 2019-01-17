31258eea0089a63216145c50c92625aec1c63417
krish9449 <krishnareddy9449@gmail.com>
2019-01-17 23:38:04 +0530

    add a method in Test class

--- Git-demo/src/com/hcl/test/Test.java
+++ Git-demo/src/com/hcl/test/Test.java
@@ -1,7 +1,13 @@
 package com.hcl.test;
 
 public class Test {
+	
+	public int add(int a, int b) {
+		return a+b;
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to GIT");
+		
 	}
 }
